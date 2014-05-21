c6e12e1244d41093f047d34818ce30fce81ea02b
tom <tom@daftscience.com>
2014-05-21 15:39:09 -0400

    added counterVariables.py file

---
+++ counterVariables.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+
+diffCells = OrderedDict()
+
+diffCells['Neutrophils']    = 'neut'
+diffCells['Bands Cells']    = 'band'
+diffCells['Lymphocytes']    = "lymph"
+diffCells['Monocytes']      = "mono"
+diffCells['Eosinophils']    = "eos"
+diffCells['Basophils']      = "baso"
+diffCells['Metamyelocytes'] = "meta"
+diffCells['Myelocytes']     = "myelo"
+diffCells['Promyelocytes']  = "pro"
+diffCells['Blasts']         = "blast"
+diffCells['NRBCs']          = "nrbc"
+diffCells['Others']         = "other"
+diffCells['Megakaryocytes'] = "mega"
+diffCells['Plasma Cells']   = "plasma"
+
+diffKeys = OrderedDict()
+diffKeys['Neutrophils']     =  "2"
+diffKeys['Bands Cells']     =  "1"
+diffKeys['Lymphocytes']     =  "3"
+diffKeys['Monocytes']       =  "6"
+diffKeys['Eosinophils']     =  "5"
+diffKeys['Basophils']       =  "4"
+diffKeys['Metamyelocytes']  =  "7"
+diffKeys['Myelocytes']      =  "8"
+diffKeys['Promyelocytes']   =  "9"
+diffKeys['Blasts']          =  " . "
+diffKeys['NRBCs']           =  "0 -zero"
+diffKeys['Others']          =  "o -letter"
+diffKeys['Megakaryocytes']  =  "*"
+diffKeys['Plasma Cells']    =  "/" 
